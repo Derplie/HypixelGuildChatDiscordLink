f09a681c332eba53d0567b165bf407ad0c2b0ea8
Derplie <78589031+Derplie@users.noreply.github.com>
2022-03-04 19:28:11 +0000

    Update HypixelBot.py

--- HypixelBot.py
+++ HypixelBot.py
@@ -1,13 +1,10 @@
 import discord
 from discord.ext import commands
-#from javascript import require, On
+from javascript import require, On
 import asyncio
 from profanity_filter import ProfanityFilter
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-#mineflayer = require("mineflayer","latest")
+mineflayer = require("mineflayer","latest")
 
 ############
 CHANNEL_ID = 948935430731100230
@@ -28,76 +25,76 @@ class MainApp(commands.Bot):
     
     def StartDiscordClient(self):
         self.run(self.token)
-#    def StartMinecraftClient(self):
-#        self.bot = mineflayer.createBot({
-#            "host": self.host,
-#            "port": self.port,
-#            "username": self.email,
-#            "password": self.pswd,
-#            "version": self.version,
-#            "auth": "microsoft"
-#        })
+    def StartMinecraftClient(self):
+        self.bot = mineflayer.createBot({
+            "host": self.host,
+            "port": self.port,
+            "username": self.email,
+            "password": self.pswd,
+            "version": self.version,
+            "auth": "microsoft"
+        })
     
 
-#    def Listener(self, ChannelID):
-#        self.msg = ""
-#        self.new_msg = False
-#
-#        @self.event
-#        async def on_message(message):
-#            await self.process_commands(message)
-#            if message.channel.id != ChannelID:
-#                return
-#            if message.author.name == self.user.name:
-#                return
-#            self.bot.chat(f"/gc {message.author.display_name} > {pf.censor(message.content)}")
-#        
-#        @On(self.bot, "chat")
-#        def handle(this, username, message, *args):
-#            self.splitmessage = message.split()
-#
-#            if username == self.bot.username:
-#                return
-#            if username != "Guild":
-#                return
-#            if self.splitmessage[1] == self.bot.username:
-#                return
-#
-#            if len(self.splitmessage) == 2:
-#                self.msg = f"**{username} > {message}**"
-#            else:
-#                if self.splitmessage[0] in ["[VIP]","[VIP+]","[MVP]","[MVP+]","[MVP++]"]:
-#                    self.msg = f"**{username} > {self.splitmessage[0]} {self.splitmessage[1]} {self.splitmessage[2]}** {message.split(' ', 3)[3]}"
-#                else:
-#                    self.msg = f"**{username} > {self.splitmessage[0]} {self.splitmessage[1]}** {message.split(' ', 2)[2]}"
-#            self.new_msg = True
-#        
-#        @self.command()
-#        async def say(ctx):
-#            if ctx.message.author.id == 320666320280616960:
-#                self.bot.chat(ctx.message.content.split(' ', 1)[1])
-#                await ctx.send("Sent")
-#            else:
-#                await ctx.send("no perms + L + ratio + bozo")
-#
-#        async def timer():
-#            await self.wait_until_ready()
-#            ch = self.get_channel(ChannelID)
-#
-#            while True:
-#                if self.new_msg == True:
-#                    await ch.send(self.msg)
-#                    self.new_msg = False
-#                await asyncio.sleep(0.05)
-#
-#        self.loop.create_task(coro=timer())
+    def Listener(self, ChannelID):
+        self.msg = ""
+        self.new_msg = False
+
+        @self.event
+        async def on_message(message):
+            await self.process_commands(message)
+            if message.channel.id != ChannelID:
+                return
+            if message.author.name == self.user.name:
+                return
+            self.bot.chat(f"/gc {message.author.display_name} > {pf.censor(message.content)}")
+        
+        @On(self.bot, "chat")
+        def handle(this, username, message, *args):
+            self.splitmessage = message.split()
+
+            if username == self.bot.username:
+                return
+            if username != "Guild":
+                return
+            if self.splitmessage[1] == self.bot.username:
+                return
+
+            if len(self.splitmessage) == 2:
+                self.msg = f"**{username} > {message}**"
+            else:
+                if self.splitmessage[0] in ["[VIP]","[VIP+]","[MVP]","[MVP+]","[MVP++]"]:
+                    self.msg = f"**{username} > {self.splitmessage[0]} {self.splitmessage[1]} {self.splitmessage[2]}** {message.split(' ', 3)[3]}"
+                else:
+                    self.msg = f"**{username} > {self.splitmessage[0]} {self.splitmessage[1]}** {message.split(' ', 2)[2]}"
+            self.new_msg = True
+        
+        @self.command()
+        async def say(ctx):
+            if ctx.message.author.id == 320666320280616960:
+                self.bot.chat(ctx.message.content.split(' ', 1)[1])
+                await ctx.send("Sent")
+            else:
+                await ctx.send("no perms + L + ratio + bozo")
+
+        async def timer():
+            await self.wait_until_ready()
+            ch = self.get_channel(ChannelID)
+
+            while True:
+                if self.new_msg == True:
+                    await ch.send(self.msg)
+                    self.new_msg = False
+                await asyncio.sleep(0.05)
+
+        self.loop.create_task(coro=timer())
         
 if __name__ == "__main__":
-    email = os.getenv("EMAIL")
-    pswd = os.getenv("PSWD")
-    token = os.getenv("TOKEN")
+    email = os.environ["EMAIL"]
+    pswd = os.environ["PSWD"]
+    token = os.environ["TOKEN"]
     App = MainApp(host="hypixel.net",port=25565,email=email,password=pswd,version="1.8.9",token=token)
-    #App.StartMinecraftClient()
-    #App.Listener(CHANNEL_ID)
+    App.StartMinecraftClient()
+    App.Listener(CHANNEL_ID)
     App.StartDiscordClient()
 
